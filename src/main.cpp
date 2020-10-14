ca247fe8f460ed10e620c534e9423ce6debe8588
Franz Miltz <franz@miltz.me>
2020-10-14 21:17:10 +0100

    Added TRM main and included them in main main

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,27 @@
 #include "main.hpp"
 
-int main(void) {
-  std::cout << "Hello World" << std::endl;
+#include "thermometer/main.hpp"
+#include "utils/concurrent/thread.hpp"
+#include "utils/logger.hpp"
+#include "utils/system.hpp"
+
+using hyped::utils::Logger;
+using hyped::utils::System;
+using hyped::utils::concurrent::Thread;
+
+int main(int argc, char **argv) {
+  System::parseArgs(argc, argv);
+  System &sys = System::getSystem();
+  sys.debug = 4;
+  sys.verbose = true;
+  Logger log(sys.verbose, sys.debug);
+
+  log.DBG("MAIN", "DBG0");
+  log.DBG1("MAIN", "DBG1");
+  log.DBG2("MAIN", "DBG2");
+  log.DBG3("MAIN", "DBG3");
+
+  Thread *trm = new oven::thermometer::Main(0, log);
+  trm->start();
+  trm->join();
 }
